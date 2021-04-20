6265b344f955a7611babb4f099bcf3a097a976e0
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-04-20 13:56:52 +0200

    update examples scripts

--- examples/example_script.py
+++ examples/example_script.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from metran import Metran
+import metran
 
+# read data
 series = []
 nts = 5
 for s in range(nts):
@@ -9,18 +10,26 @@ for s in range(nts):
                      parse_dates=True, infer_datetime_format=True,
                      dayfirst=True)
     series.append(ts)
-mt = Metran(series, name="B21B0214")
+
+# create model
+mt = metran.Metran(series, name="B21B0214")
 mt.solve()
+
 # Get eigenvalues (can be used to plot scree plot, see e.g. Fig 2 in JoH paper)
 eigval = mt.eigval
+
 # Get all smoothed state means
 states = mt.get_state_means()
+
 # Get all (smoothed) projected state means
 means = mt.get_projected_means()
+
 # Get projected mean for specific series with/without confidence interval
 proj = mt.get_projection("B21B0214005", ci=True)
+
 # Decomposed projected mean for specific series
 dec = mt.decompose_projection("B21B0214001")
+
 # Get specific state mean with/without confidence interval
 mt.get_state(0, ci=True)
 
