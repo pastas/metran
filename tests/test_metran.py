b9dd4d7cb0159e776894b15c60acc2886446cccf
wlberendrecht <80323130+wlberendrecht@users.noreply.github.com>
2021-04-21 15:18:59 +0200

    update test_metran
    - use updated names and arguments

--- tests/test_metran.py
+++ tests/test_metran.py
@@ -28,32 +28,32 @@ def test_metran_state_means(mt):
 
 
 def test_metran_projected_means(mt):
-    _ = mt.get_projected_means()
+    _ = mt.get_simulated_means()
     return
 
 
 def test_metran_get_projection(mt):
-    _ = mt.get_projection("B21B0214005", ci=True)
+    _ = mt.get_simulation("B21B0214005")
     return
 
 
 def test_metran_decompose_projection(mt):
-    _ = mt.decompose_projection("B21B0214001")
+    _ = mt.decompose_simulation("B21B0214001")
     return
 
 
 def test_metran_get_state(mt):
-    _ = mt.get_state(0, ci=True)
+    _ = mt.get_state(0)
     return
 
 
 def test_metran_masked_oseries(mt):
-    proj1 = mt.get_projection("B21B0214005", ci=True)
+    proj1 = mt.get_simulation("B21B0214005")
     oseries = mt.get_observations()
     mask = (0 * oseries).astype(bool)
     mask.loc["1997-8-28", "B21B0214005"] = True
     mt.mask_observations(mask)
-    proj2 = mt.get_projection("B21B0214005", ci=True)
+    proj2 = mt.get_simulation("B21B0214005")
     mt.unmask_observations()
     assert (proj1 != proj2).any().any()
     return
