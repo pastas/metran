20518b3a4530cd4c702c9c36a32e5d143bd7be2b
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-04-30 18:33:50 +0200

    update path in example script

--- examples/example_script.py
+++ examples/example_script.py
@@ -5,7 +5,7 @@ import metran
 series = []
 nts = 5
 for s in range(nts):
-    ts = pd.read_csv("examples/data/B21B021400" + str(s + 1) + "_res.csv",
+    ts = pd.read_csv("./data/B21B021400" + str(s + 1) + "_res.csv",
                      header=0, index_col=0, names=["B21B021400" + str(s + 1)],
                      parse_dates=True, infer_datetime_format=True,
                      dayfirst=True)
