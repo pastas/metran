a35a7edf8fa3b57d0ab505350ef626b701a8f905
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-05-03 15:54:55 +0200

    autopep8, sort imports

--- tests/test_factoranalysis.py
+++ tests/test_factoranalysis.py
@@ -1,14 +1,15 @@
-from metran.factoranalysis import FactorAnalysis
 import metran
-import pytest
 import numpy as np
+from metran.factoranalysis import FactorAnalysis
 
 from conftest import get_data
+
 
 def test_fa_init():
     corr = np.array([[1, 0.8], [0.8, 1]])
     fa = FactorAnalysis()
     return fa, corr
+
 
 def test_fa_eigval():
     fa, corr = test_fa_init()
@@ -16,11 +17,13 @@ def test_fa_eigval():
     assert np.allclose(eigval, np.array([1.8, 0.2]))
     return eigval, eigvec
 
+
 def test_fa_maptest():
     fa, corr = test_fa_init()
     eigval, eigvec = test_fa_eigval()
     (nfactors, _) = fa._maptest(corr, eigvec, eigval)
     assert nfactors == 1
+
 
 def test_fa_solve():
     series = get_data()
