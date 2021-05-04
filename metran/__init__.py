ccbdc5b52292f7d0be9c00b52552f0872f969090
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2021-05-04 18:04:03 +0200

    add show_versions method for printing dependency versions

--- metran/__init__.py
+++ metran/__init__.py
@@ -1,4 +1,5 @@
 from . import factoranalysis, kalmanfilter, metran, solver
 from .metran import Metran
 from .solver import LmfitSolve, ScipySolve
+from .utils import show_versions
 from .version import __version__
---
+++ metran/utils.py
@@ -0,0 +1,34 @@
+def show_versions():
+    """Method to print the version of dependencies.
+    """
+    from pastas import __version__ as ps_version
+    from pandas import __version__ as pd_version
+    from numpy import __version__ as np_version
+    from scipy import __version__ as sc_version
+    from matplotlib import __version__ as mpl_version
+    from sys import version as os_version
+
+    msg = (
+        f"Python version: {os_version}\n"
+        f"numpy version: {np_version}\n"
+        f"scipy version: {sc_version}\n"
+        f"pandas version: {pd_version}\n"
+        f"matplotlib version: {mpl_version}\n"
+        f"pastas version: {ps_version}"
+    )
+
+    # numba
+    try:
+        from numba import __version__ as nb_version
+        msg = msg + f"\nnumba version: {nb_version}"
+    except ModuleNotFoundError:
+        msg = msg + "\nnumba version: not installed"
+
+    # lmfit
+    try:
+        from lmfit import __version__ as lm_version
+        msg = msg + f"\nlmfit version: {lm_version}"
+    except ModuleNotFoundError:
+        msg = msg + "\nlmfit version: not installed"
+
+    return print(msg)
