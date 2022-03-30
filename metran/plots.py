4c3742fddd0e3eb27174f3115e45835490bb4378
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2022-03-30 14:17:33 +0200

    only import version

--- metran/plots.py
+++ metran/plots.py
@@ -2,8 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pandas import Timestamp
-import pastas
-if pastas.__version__ < '0.19.0':
+from pastas import __version__ as psversion
+
+if psversion < '0.19.0':
     from pastas.plots import _get_height_ratios
 else:
     from pastas.modelplots import _get_height_ratios
