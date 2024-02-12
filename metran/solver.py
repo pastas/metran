469d762b8beaedf130ba649ac6e87ba2a8893e90
Martin Vonk <vonk.mart@gmail.com>
2024-02-12 20:25:50 +0100

    fix #24 NumPy matrix deprecation

--- metran/solver.py
+++ metran/solver.py
@@ -182,11 +182,11 @@ class BaseSolver:
         """
         n = A.shape[0]
         eigval, eigvec = np.linalg.eig(A)
-        val = np.matrix(np.maximum(eigval, epsilon))
-        vec = np.matrix(eigvec)
+        val = np.array(np.maximum(eigval, epsilon))
+        vec = np.array(eigvec)
         with np.errstate(divide="ignore", invalid="ignore"):
             T = 1 / (np.multiply(vec, vec) * val.T)
-            T = np.matrix(np.sqrt(np.diag(np.array(T).reshape((n)))))
+            T = np.array(np.sqrt(np.diag(np.array(T).reshape((n)))))
             B = T * vec * np.diag(np.array(np.sqrt(val)).reshape((n)))
         out = B * B.T
         return out
