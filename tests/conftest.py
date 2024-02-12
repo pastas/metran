85236810921f3b5372b65c67bafc9ca29949a23a
Martin Vonk <vonk.mart@gmail.com>
2024-02-12 18:48:26 +0100

    updating pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,43 @@
 import os
-
-import pandas as pd
 import pytest
+from pathlib import Path
+from typing import List
+from pandas import read_csv, Series
+from numpy import array, ndarray
 
 import metran
 
 
-def get_data():
-    datadir = "./examples/data"
-    series = []
-    files = [
-        os.path.join(datadir, f) for f in os.listdir(datadir) if f.endswith("_res.csv")
-    ]
-    files.sort()
-    for fi in files:
-        name = fi.split(os.sep)[-1].split(".")[0].split("_")[0]
-        ts = pd.read_csv(
-            fi,
-            header=0,
-            index_col=0,
-            parse_dates=True,
-            date_format="%Y-%m-%d",
-            names=[name],
-        )
-        series.append(ts)
-    return series
+seriesl = [
+    read_csv(
+        fi,
+        header=0,
+        index_col=0,
+        parse_dates=True,
+        date_format="%Y-%m-%d",
+        names=[fi.stem.split("_")[0]],
+    )
+    for fi in Path("../examples/data").glob("*_res.csv")
+]
 
 
-@pytest.fixture(scope="module")
-def mt(request):
-    """Fixture that yields metran object"""
-    series = get_data()
-    mt = metran.Metran(series, name="B21B0214")
-    mt.solve()
-    yield mt
+@pytest.fixture
+def series_list() -> List[Series]:
+    return seriesl
+
+
+@pytest.fixture
+def mt_init(seriesl) -> metran.Metran:
+    """Fixture that yields initialized metran object"""
+    return metran.Metran(seriesl, name="B21B0214")
+
+
+@pytest.fixture
+def mt(mt_init) -> metran.Metran:
+    """Fixture that yields solved metran object"""
+    return mt_init.solve()
+
+
+@pytest.fixture
+def corr() -> ndarray[float]:
+    return array([[1.0, 0.8], [0.8, 1.0]], dtype=float)
