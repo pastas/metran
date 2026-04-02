11635f9f639e8e99decd575f5c4cc6991053c26b
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-04-02 12:34:28 +0200

    set version for release

--- metran/version.py
+++ metran/version.py
@@ -2,4 +2,4 @@
 
 # This is the only location where the version will be written and changed.
 # Based on https://packaging.python.org/single_source_version/
-__version__ = "0.5.0.dev0"
+__version__ = "0.5.0"
